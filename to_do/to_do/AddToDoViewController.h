b29dc76bcdfe6f761c185adabe9d6cfd718f2cb2
White, Jordan <jwhite3@gannett.com>
2014-07-31 11:58:13 -0400

    initial commit

---
+++ to_do/to_do/AddToDoViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddToDoViewController.h
+//  to_do
+//
+//  Created by White, Jordan on 6/14/14.
+//  Copyright (c) 2014 Gannett Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ToDoItem.h"
+
+
+@interface AddToDoViewController : UIViewController
+
+@property ToDoItem *toDoItem;
+
+@end
---
+++ to_do/to_do/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  to_do
+//
+//  Created by White, Jordan on 6/13/14.
+//  Copyright (c) 2014 Gannett Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
